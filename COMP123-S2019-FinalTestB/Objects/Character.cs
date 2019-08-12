923b430cc4e085b85cc962efe011cf2d27a98cd4
301045522 <301045522@PR-E216-17>
2019-08-12 12:52:58 -0400

    added the character and item classes

---
+++ COMP123-S2019-FinalTestB/Objects/Character.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * STUDENT NAME:
+ * STUDENT ID:
+ * DESCRIPTION: This is the character class used in character creation as well as the data container for the application
+ * */
+
+namespace COMP123_S2019_FinalTestB.Objects
+{
+    class Character
+    {
+        //Character Abilities
+        public string Strength { get; set; }
+        public string Dexterity { get; set; }
+        public string Constitution { get; set; }
+        public string Intelligence { get; set; }
+        public string Wisdom { get; set; }
+        public string Charisma { get; set; }
+
+        //Secondary Abilities
+        public int ArmourClass { get; set; }
+        public int HitPoints { get; set; }
+
+        //Character Class
+        public int CharacterClass { get; set; }
+        public int Level { get; set; }
+
+        //Equipment
+        List<Item> Inventory;
+
+        //Constructor 
+        Character()
+        {
+            this.Inventory = new List<Item>();  
+        }
+    }
+}
